158060d8b51c6ae9b3282ed1b185efd8c38c0317
pedro quintas <pedroquintas1213@gmail.com>
2025-11-05 18:15:23 -0300

    rollback to version 0.2.1

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import config.logger   # inicializa logging
 from app import create_app
 import threading, webbrowser
+from model.companieModel import companie
 
 app = create_app()
 
 def open_browser():
-    webbrowser.open_new('http://127.0.0.1:5000/login')
+    with app.app_context():
+        has_company = companie.query.first() is not None
+
+    target = 'http://127.0.0.1:5000/login' if has_company else 'http://127.0.0.1:5000/cadastroCompanie'
+    
+    webbrowser.open_new(target)
 
 if __name__ == '__main__':
-    threading.Timer(1.0, open_browser).start()
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    # Dispara o open_browser em thread separada
+    threading.Timer(0.5, open_browser).start()
+    app.run(host='0.0.0.0', port=5000, debug=True)
